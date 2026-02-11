c255a986ddedc39e91f220d0b3029588a49c2b80
peeeffchang <peeeffchang@gmail.com>
2026-02-11 18:00:51 +0800

    Revert "Protocol Engine for State Machine Logic"
    This reverts commit a417583ecdadcf0a1167e00db5b9c00b8cccbe61.

--- engine/__init__.py
+++
@@ -1,7 +0,0 @@
-# engine/__init__.py
-from .statemachine import Action, State, Transition, StateMachine
-from .statemachine_loader import StateMachineLoader
-from .action_envelope import IntentBasedActionEnvelope, Constraint, Signature
-from .action_parser import ActionParser
-from .validator import ProtocolValidator
-from .capabilities_loader import CapabilitiesRegistry, Capability
--- tests/engine/__init__.py
+++
@@ -1,1 +0,0 @@
-# tests/engine/__init__.py
